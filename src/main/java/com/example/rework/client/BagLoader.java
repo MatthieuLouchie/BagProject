6d028a9b9efdf82f072f00a52963377af91a9f8d
MatthieuLouchie <matthieu.louchie@sfr.fr>
2021-12-27 09:26:20 +0100

    Create multiple bags with a JSON file

--- src/main/java/com/example/rework/client/BagLoader.java
+++ src/main/java/com/example/rework/client/BagLoader.java
@@ -2,6 +2,8 @@ package com.example.rework.client;
 
 import com.example.rework.model.Bag;
 import com.example.rework.service.BagService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,6 +11,7 @@ import java.util.List;
 
 public class BagLoader {
     private final BagService bagService = new BagService();             //Nouvel objet
+    private final ObjectMapper objectMapper = new ObjectMapper();
     public void loadfromfile(String file) throws IOException {
 
         BufferedReader in = new BufferedReader(new FileReader(file));   //lire le fichier .txt
@@ -21,7 +24,37 @@ public class BagLoader {
             line = in.readLine();                                       //prendre la ligne suivante
         }
     }
+
     public List<Bag> get(){
         return bagService.get();
+    }
+
+    public void loadFromJSonFile(String json) throws IOException {
+        BufferedReader bagParameters = new BufferedReader(new FileReader(json));
+
+        JsonNode jsonNode = objectMapper.readTree(bagParameters);
+        String name = jsonNode.get("name").asText();
+        String type = jsonNode.get("type").asText();
+
+        bagService.create(name, type);                      //passer les deux variables dans la fonction create du BagService.java
+    }
+
+    public void loadSomeBagsFromJSonFile(String json) throws IOException {
+        BufferedReader bagParameters = new BufferedReader(new FileReader(json));        //lecture du fichier json
+        JsonNode jsonNode = objectMapper.readTree(bagParameters);                       //
+
+        String jsonValue = jsonNode.toString();                                         //conversion JsonNode -> String
+        String[] parts = jsonValue.split("}");                                   //découpe des 
+
+
+        for(int i = 0; i<=(parts.length)-2; i++){
+            parts[i] = parts[i].substring(1) + '}';
+            JsonNode jsn = objectMapper.readTree(parts[i]);
+
+            String name = jsn.get("name").asText();
+            String type = jsn.get("type").asText();
+
+            bagService.create(name, type);                      //passer les deux variables dans la fonction create du BagService.java
+        }
     }
 }
--- src/test/java/com/example/rework/BagLoaderTest.java
+++ src/test/java/com/example/rework/BagLoaderTest.java
@@ -22,4 +22,34 @@ class BagLoaderTest {
                 .extracting(Bag::getName)
                 .contains("MyBag1", "MyBag2", "MyBag3", "MyBag4");     //verify if the entire list contains my bags
     }
+
+    @Test
+    void shouldAddBagsFromJsonFile() throws IOException {
+
+        // given
+        BagLoader bagLoader = new BagLoader();
+
+        // when
+        bagLoader.loadFromJSonFile("src/main/resources/bag.json");
+
+        // then
+        Assertions.assertThat(bagLoader.get())
+                .extracting(Bag::getName)
+                .contains("MyBag1");     //verify if the entire list contains my bags
+    }
+
+    @Test
+    void shouldAddSomeBagsFromJsonFile() throws IOException {
+
+        // given
+        BagLoader bagLoader = new BagLoader();
+
+        // when
+        bagLoader.loadSomeBagsFromJSonFile("src/main/resources/bags.json");     //json file with multiple bags
+
+        // then
+        Assertions.assertThat(bagLoader.get())
+                .extracting(Bag::getName)
+                .contains("MyBag1", "MyBag2", "MyBag3", "MyBag4");              //verify if the entire list contains my bags
+    }
 }
